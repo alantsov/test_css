36bdbfa5e343090be6c0824ae6e82398f3b5d39a
Anton <anton.lantsov@gmail.com>
2013-04-11 15:51:07 +0400

    test css

---
+++ test_css.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+readonly MOCK=~/Pictures/email_sent_mock.png
+readonly GEOMETRY='601x362'
+
+google-chrome $1 &
+sleep 7
+gnome-screenshot -c
+./pasteimg ~/Pictures/temp.png
+convert ~/Pictures/temp.png -crop $GEOMETRY+706+334 ~/Pictures/cropped_temp.png
+compare ~/Pictures/cropped_temp.png $MOCK ~/Pictures/difference.png
+convert ~/Pictures/cropped_temp.png $MOCK -compose Overlay   -composite ~/Pictures/overlay.png
+cd ~/Pictures
+montage cropped_temp.png $MOCK difference.png overlay.png -tile 2x2 -geometry $GEOMETRY+1+1 result.png
