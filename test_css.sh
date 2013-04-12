cc766084a207eec249c4c8c9aa2b9c3954985d86
Anton <anton.lantsov@gmail.com>
2013-04-12 16:22:36 +0400

    ./test_css.sh config1.cfg config2.cfg

--- test_css.sh
+++ test_css.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-readonly MOCK=main_redeem_mock.png
-readonly GEOMETRY='782x469'
-readonly X='705'
-readonly Y='334'
-
 readonly DELAY=7
 readonly TEMP_DIR=~/Pictures
 readonly TEMP_FILE=temp.png
 readonly CROPPED_FILE=cropped_temp.png
 readonly DIFF_FILE=difference.png
 readonly OVERLAY_FILE=overlay.png
-readonly RESULT_FILE=result.png
 
-google-chrome $1 &
-sleep $DELAY
-gnome-screenshot -c
-./pasteimg $TEMP_DIR/$TEMP_FILE
+for config in "$@"; do
+  source $config
+  google-chrome $URL &
+  sleep $DELAY
+  gnome-screenshot -c
+  ./pasteimg $TEMP_DIR/$TEMP_FILE
+  current_dir=$PWD
+  cd $TEMP_DIR
+  convert $TEMP_FILE -crop $GEOMETRY+$X+$Y $CROPPED_FILE
+  compare $CROPPED_FILE $MOCK $DIFF_FILE
+  convert $CROPPED_FILE $MOCK -compose Overlay -composite $OVERLAY_FILE
+  montage $CROPPED_FILE $MOCK $DIFF_FILE $OVERLAY_FILE -tile 2x2 -geometry $GEOMETRY+1+1 $RESULT_FILE
+  cd $current_dir
+done
+
 cd $TEMP_DIR
-convert $TEMP_FILE -crop $GEOMETRY+$X+$Y $CROPPED_FILE
-compare $CROPPED_FILE $MOCK $DIFF_FILE
-convert $CROPPED_FILE $MOCK -compose Overlay -composite $OVERLAY_FILE
-montage $CROPPED_FILE $MOCK $DIFF_FILE $OVERLAY_FILE -tile 2x2 -geometry $GEOMETRY+1+1 $RESULT_FILE
+rm $TEMP_FILE
+rm $CROPPED_FILE
+rm $DIFF_FILE
+rm $OVERLAY_FILE
